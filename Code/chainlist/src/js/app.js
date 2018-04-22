4f674b18e54b3688eed20611ca9e02bfd8a30359
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2018-04-22 21:27:39 +0200

    Article tests

--- Code/chainlist/src/js/app.js
+++ Code/chainlist/src/js/app.js
@@ -4,30 +4,17 @@ App = {
   account: 0x0,
 
   init: function() {
-    // load articlesRow
-    var articlesRow = $("#articlesRow");
-    var articleTemplate = $("#articleTemplate");
-
-    articleTemplate.find(".panel-title").text("article 1");
-    articleTemplate
-      .find(".article-description")
-      .text("Desription for article 1");
-    articleTemplate.find(".article-price").text("10.23");
-    articleTemplate.find(".article-seller").text("0x1234567890123456890");
-
-    articlesRow.append(articleTemplate.html());
-
     return App.initWeb3();
   },
 
   initWeb3: function() {
     // initialize web3
-    if (typeof web3 !== "undefined") {
+    if(typeof web3 !== 'undefined') {
+      //reuse the provider of the Web3 object injected by Metamask
       App.web3Provider = web3.currentProvider;
     } else {
-      App.web3Provider = new Web3.providers.HttpProvider(
-        "http://localhost:7545"
-      );
+      //create a new provider and plug it directly into our local node
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
     web3 = new Web3(App.web3Provider);
 
@@ -38,88 +25,85 @@ App = {
 
   displayAccountInfo: function() {
     web3.eth.getCoinbase(function(err, account) {
-      if (err === null) {
+      if(err === null) {
         App.account = account;
-        $("#account").text(account);
+        $('#account').text(account);
         web3.eth.getBalance(account, function(err, balance) {
-          if (err === null) {
-            $("#accountBalance").text(web3.fromWei(balance, "ether") + " ETH");
+          if(err === null) {
+            $('#accountBalance').text(web3.fromWei(balance, "ether") + " ETH");
           }
-        });
+        })
       }
     });
   },
 
   initContract: function() {
-    $.getJSON("ChainList.json", function(chainListArtifact) {
-      // get contract rtifact file and use it to instantiate  truffle contract abstraction
+    $.getJSON('ChainList.json', function(chainListArtifact) {
+      // get the contract artifact file and use it to instantiate a truffle contract abstraction
       App.contracts.ChainList = TruffleContract(chainListArtifact);
-      // set provider for contract
+      // set the provider for our contracts
       App.contracts.ChainList.setProvider(App.web3Provider);
-      // retrieve article from the contract
+      // retrieve the article from the contract
       return App.reloadArticles();
     });
   },
 
   reloadArticles: function() {
+    // refresh account information because the balance might have changed
     App.displayAccountInfo();
-    $("#articlesRow").empty();
 
-    App.contracts.ChainList.deployed()
-      .then(function(instance) {
-        return instance.getArticle();
-      })
-      .then(function(article) {
-        if (article[0] == 0x0) {
-          // no article
-          return;
-        }
+    // retrieve the article placeholder and clear it
+    $('#articlesRow').empty();
 
-        var articleTemplate = $("#articleTemplate");
+    App.contracts.ChainList.deployed().then(function(instance) {
+      return instance.getArticle();
+    }).then(function(article) {
+      if(article[0] == 0x0) {
+        // no article
+        return;
+      }
 
-        articleTemplate.find(".panel-title").text(article[1]);
-        articleTemplate.find(".panel-description").text(article[2]);
-        articleTemplate
-          .find(".panel-price")
-          .text(web3.fromWei(article[3]), "ether");
+      // retrieve the article template and fill it
+      var articleTemplate = $('#articleTemplate');
+      articleTemplate.find('.panel-title').text(article[1]);
+      articleTemplate.find('.article-description').text(article[2]);
+      articleTemplate.find('.article-price').text(web3.fromWei(article[3], "ether"));
 
-          var seller = article[0];
-          if (seller == App.account) {
-            seller = "You";
-          }
+      var seller = article[0];
+      if (seller == App.account) {
+        seller = "You";
+      }
+      articleTemplate.find('.article-seller').text(seller);
 
-          articleTemplate.find('.article-seller').text(seller);
-
-          $('#articlesRow').append(articleTemplate.html());
-      })
-      .catch(function(err) {
-        console.error(err.message);
-      });
+      // add this article
+      $('#articlesRow').append(articleTemplate.html());
+    }).catch(function(err) {
+      console.error(err.message);
+    });
   },
 
   sellArticle: function() {
+    // retrieve the detail of the article
     var _article_name = $('#article_name').val();
     var _description = $('#article_description').val();
-    var _price = web3.fromWei(parseFloat($('#article_price').val() || 0), "ether");
+    var _price = web3.toWei(parseFloat($('#article_price').val() || 0), "ether");
 
-    if ((_article_name.trim() == '') || (_price == 0)) {
+    if((_article_name.trim() == '') || (_price == 0)) {
+      // nothing to sell
       return false;
     }
 
-    App.contracts.ChainList.deployed()
-    .then(function (instance) {
+    App.contracts.ChainList.deployed().then(function(instance) {
       return instance.sellArticle(_article_name, _description, _price, {
         from: App.account,
         gas: 500000
       });
-    })
-    .then(function (result) {
+    }).then(function(result) {
       App.reloadArticles();
-    })
-    .catch(function (err) {
-      console.error(err.message);
+    }).catch(function(err) {
+      console.error(err);
     });
-  }
+  },
 };
 
 $(function() {
--- Code/chainlist/test/ChainListHappyPath.js
+++ Code/chainlist/test/ChainListHappyPath.js
@@ -1,47 +1,55 @@
 const ChainList = artifacts.require("./ChainList.sol");
 
 contract("ChainList", accounts => {
-  let chainListInstance;
-  let seller = accounts[1];
-  let articleName = "article 1";
-  let articleDescription = "Description for article 1";
-  let articlePrice = 10;
+    let chainListInstance;
+    let seller = accounts[1];
+    let articleName = "article 1";
+    let articleDescription = "Description for article 1";
+    let articlePrice = 10;
 
-  it("should be initialized with empty values", () => {
-    return ChainList.deployed()
-      .then(instance => {
-        return instance.getArticle();
-      })
-      .then(data => {
-        assert.equal(data[0], 0x0, "seller must be empty");
-        assert.equal(data[1], "", "article name must be empty");
-        assert.equal(data[2], "", "article description must be empty");
-        assert.equal(data[3].toNumber(), 0, "article price must be zero");
-      });
-  });
+    it("should be initialized with empty values", () => {
+        return ChainList.deployed().then(instance => {
+            return instance.getArticle();
+        }).then(data => {
+            assert.equal(data[0], 0x0, "seller must be empty");
+            assert.equal(data[1], "", "article name must be empty");
+            assert.equal(data[2], "", "article description must be empty");
+            assert.equal(data[3].toNumber(), 0, "article price must be zero");
+        });
+    });
 
-  it("should sell article", () =>
-    ChainList.deployed(instance => {
-      chainListInstance = instance;
-      return chainListInstance
-        .sellArticle(
-          articleName,
-          articleDescription,
-          web3.toWei(articlePrice, "ether"),
-          {
-            from: seller
-          }
-        )
-        .then(() => chainListInstance.getArticle())
-        .then(data => {
-          assert.equal(data[0], seller, "seller must be " + seller);
-          assert.equal(data[1], articleName, "article name  must be empty");
-          assert.equal(data[2], articleDescription, "article description must be " + articleDescription);
-          assert.equal(
-              data[3].toNumber(),
-              web3.toWei(articlePrice, "ether"),
-              "article price must be " + web3.toWei(articlePrice, "ether")
-            );
+    it("should sell article", () => ChainList.deployed(instance => {
+        chainListInstance = instance;
+        return chainListInstance.sellArticle(articleName, articleDescription, web3.toWei(articlePrice, "ether"), {from: seller}).then(() => chainListInstance.getArticle()).then(data => {
+            assert.equal(data[0], seller, "seller must be " + seller);
+            assert.equal(data[1], articleName, "article name  must be empty");
+            assert.equal(data[2], articleDescription, "article description must be " + articleDescription);
+            assert.equal(data[3].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
         });
     }));
+
+    it('should trigger an event when new article is sold', () => {
+        return ChainList.deployed().then((instance) => {
+            chainListInstance = instance;
+            return chainListInstance.sellArticle(articleName, articleDescription, web3.toWei(articlePrice, "ether"), {from: seller})
+			.then((receipt) => {
+				assert.equal(
+                    receipt.logs.length, 1,
+                    "one ebent shouldn't been triggered");
+				assert.equal(
+                    receipt.logs[0].event, "LogSellArticle",
+                    "Event SHould be LogSellArticle");
+				assert.equal(
+                    receipt.logs[0].args._seller, seller,
+                    "event seller must be " + seller);
+                assert.equal(
+                    receipt.logs[0].args._name, articleName,
+                    "event article must be " + articleName);
+				assert.equal(
+                    receipt.logs[0].args._price.toNumber(),
+                    web3.toWei(articlePrice,'ether'),
+                    "event seller must be " + articlePrice);
+			})
+        });
+    })
 });
