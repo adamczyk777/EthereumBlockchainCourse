d5194fac81cb960f708446c7b097061a0488555b
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2018-05-24 23:33:37 +0200

    add new contract function to byu article

--- Code/chainlist/contracts/ChainList.sol
+++ Code/chainlist/contracts/ChainList.sol
@@ -3,11 +3,19 @@ pragma solidity ^0.4.18;
 contract ChainList {
     // state variables
     address seller;
+    address buyer;
     string name;
     string description;
     uint256 price;
 
     // events:
+
+    event LogBuyArticle(
+        address indexed _seller,
+        address indexed _buyer,
+        string _name,
+        uint256 _price
+    );
 
     event LogSellArticle(
         address indexed _seller,
@@ -28,10 +36,29 @@ contract ChainList {
     // get an article
     function getArticle() public view returns (
         address _seller,
+        address _buyer,
         string _name,
         string _description,
         uint256 _price
     ) {
-        return(seller, name, description, price);
+        return(seller, buyer, name, description, price);
+    }
+
+    // buy an article contract
+    function buyArticle() payable public {
+        // check whether there is an article for sale
+        require(seller != 0x0);
+        // we check whether the article has not been sold yet
+        require(buyer == 0x0);
+        // we don't allow the seller to buy his own article
+        require(msg.sender != seller);
+        // we check that the value sent corresponds to the price of the article
+        require(msg.value == price);
+        // keep track of buyer info
+        buyer = msg.sender;
+        // the buyer can pay the seller
+        seller.transfer(msg.value);
+
+        LogBuyArticle(seller, buyer, name, price);
     }
 }
